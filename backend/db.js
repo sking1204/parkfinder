a8fd05e428b87311a37185d4bca3dde704bb458e
Shanaz King <sking1128@YewYew1128.localdomain>
2024-08-05 12:34:21 -0400

    updating congig

--- backend/db.js
+++ backend/db.js
@@ -1,21 +1,39 @@
 // //Another workaround - works locally
+// "use strict";
+// /** Database setup for jobly. */
+// const { Client } = require("pg");
+// const { getDatabaseUri } = require("./config");
+
+// let DB_URI;
+
+// if (process.env.NODE_ENV === "test") {
+//   DB_URI = "postgresql:///parkfinder_test";
+// } else {
+//   DB_URI = "postgresql:///parkfinder";
+// }
+
+// let db = new Client({
+//   connectionString: DB_URI
+// });
+
+// db.connect();
+
+// module.exports = db;
+
 "use strict";
 /** Database setup for jobly. */
 const { Client } = require("pg");
 const { getDatabaseUri } = require("./config");
 
-let DB_URI;
-
-if (process.env.NODE_ENV === "test") {
-  DB_URI = "postgresql:///parkfinder_test";
-} else {
-  DB_URI = "postgresql:///parkfinder";
-}
-
 let db = new Client({
-  connectionString: DB_URI
+  connectionString: getDatabaseUri(),
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-db.connect();
+db.connect()
+  .then(() => console.log('Connected to the database'))
+  .catch(err => console.error('Database connection error', err.stack));
 
 module.exports = db;
