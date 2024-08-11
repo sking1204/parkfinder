490db1e7f831cb6eaf417034bc6f1d37a328a295
Shanaz King <sking1128@YewYew1128.localdomain>
2024-08-11 02:26:31 -0400

    adding div to component

--- frontend/react-parkfinder/src/components/PageNotFound.jsx
+++ frontend/react-parkfinder/src/components/PageNotFound.jsx
@@ -35,9 +35,11 @@ const PageNotFound = () => {
                         </Typography>
                     </CardContent> 
                     </Card>
-                    </div>         
+                    </div> 
+                    <div>        
      
                     <Link className="back" to={`/`} >Back to Parkfinder!</Link>
+                    </div>
     </div>
   );
 }
